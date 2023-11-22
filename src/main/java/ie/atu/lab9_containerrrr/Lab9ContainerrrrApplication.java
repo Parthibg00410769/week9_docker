aeec8e15b53a73e206028085fcdb3e5803259066
Parthib007 <G00410769@atu.ie>
2023-11-22 15:52:48 +0000

    docker is set

---
+++ src/main/java/ie/atu/lab9_containerrrr/Lab9ContainerrrrApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab9_containerrrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab9ContainerrrrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab9ContainerrrrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab9_containerrrr/Lab9ContainerrrrApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab9_containerrrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab9ContainerrrrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
